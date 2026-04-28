841bbf65bb88b151f2206b9ae07ee459c688fc17
Avinash <avinashjadhav80100@gmail.com>
2026-04-28 12:35:28 +0530

    update attendanceRoute

--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const attendanceController = require("../controllers/attendanceController");
 const isAdmin = require("../middlewares/isAdmin");
 const { isStudent } = require("../middlewares/studentAuth");
+const { setHeader } = require("../middlewares/setHeader");
 
 // 🔥 Python → Node
 router.post("/mark-face", attendanceController.markAttendanceByFace);
@@ -12,6 +13,6 @@ router.post("/mark-face", attendanceController.markAttendanceByFace);
 router.get("/view",  isAdmin, attendanceController.viewAttendance);
 
 // 🔥 STUDENT
-router.get("/student", isStudent, attendanceController.studentAttendancePage);
+router.get("/student", isStudent,setHeader(true), attendanceController.studentAttendancePage);
 
 module.exports = router;
